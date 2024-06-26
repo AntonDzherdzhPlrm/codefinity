00bd29badfa0f9d59f3287cd87f79f73e9bcef76
Anton Dzherdzh <a.dzherdzh@plarium.com>
2024-06-26 13:12:12 +0200

    commit
    add auth
    
    fix

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -4,6 +4,7 @@ import {
   UsersIcon,
   ClipboardDocumentIcon,
 } from '@heroicons/react/24/outline';
+import { fetchCardData } from '@/app/lib/data';
 import { rubik } from '@/app/ui/fonts';
 
 const iconMap = {
@@ -14,14 +15,21 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
+  const {
+    totalFulfilledInvoices,
+    totalAwaitingInvoices,
+    numberOfInvoices,
+    numberOfSellers,
+  } = await fetchCardData();
+
   return (
     <>
       {/* Attention! Uncomment this section when you reach this stage in the course. */}
 
-      {/* <Card title="Earned" value={totalFulfilledInvoices} type="earned" />
+      <Card title="Earned" value={totalFulfilledInvoices} type="earned" />
       <Card title="In Progress" value={totalAwaitingInvoices} type="awaiting" />
       <Card title="All Invoices" value={numberOfInvoices} type="invoices" />
-      <Card title="Total Sellers" value={numberOfSellers} type="sellers" /> */}
+      <Card title="Total Sellers" value={numberOfSellers} type="sellers" />
     </>
   );
 }
